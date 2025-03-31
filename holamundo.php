638d613dae51dd810b00ecac68317ef5d3e1bc85
Adriandam1 <aabeijoncarbajo@danielcastelao.org>
2025-03-31 12:16:02 +0200

    variable y booleano

--- holamundo.php
+++ holamundo.php
@@ -13,8 +13,23 @@
 	// comentario una sola linea
 	/*Comentarios varias lineas */
 	# Comentario consola
+	// Variable string
+	$prueba = "variable prueba";
+	// Variable integer
+	$numero = 5;
+	$Frase = "la frase es $prueba y $numero";
+	$ProfesorTrue = true;
+	$ProfesorFalse = false;
+
+
 	print "<br/>Esto linea la hago con print";
-	print "<br/>Esto linea la hago con print para hacer otro commit";
+	print "<br/>Esto linea la hago con print para hacer otro commit $prueba$numero<br/>";
+	print $Frase;
+	print "<br/>Contaneo texto con variable" . $Frase;
+	print "<br/>Si el booleano es True mostrara1: ". $ProfesorTrue;
+	print "<br/>Si el booleano es False no mostrara nada: ". $ProfesorFalse;
+	print "<br/>Si el booleano es verdadero sale: " . json_encode ($ProfesorTrue);
+	print "<br/>Si el booleano es falso sale: " . json_encode ($ProfesorFalse);
 ?>
 </body>
 </html>
