59a5daa847ab7c9fffb155a5d7a471504bcd2abb
Adriandam1 <aabeijoncarbajo@danielcastelao.org>
2025-03-31 11:23:43 +0200

    commit inicial

---
+++ holamundo.php
@@ -0,0 +1,20 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Probando HOla mundo</title>
+</head>
+
+<body>
+<?php
+	echo "HOLA BIENVENIDO actualizate con un f5 lamp!";
+	//salto lineaecho "<br>";
+	// cambios para commit
+
+	// comentario una sola linea
+	/*Comentarios varias lineas */
+	# Comentario consola
+	print "<br/>Esto linea la hago con print";
+?>
+</body>
+</html>
